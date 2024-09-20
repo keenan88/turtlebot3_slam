b628e69bacccb2ab0fe368fcbd3a0aa411d0e366
Keenan88 <lakingsfan08@gmail.com>
2024-09-20 11:19:14 -0400

    Full world spawning now

--- docker/turtlebot3/entry_scripts/tb3_gz_slam_or_nav_entrypoint.sh
+++ docker/turtlebot3/entry_scripts/tb3_gz_slam_or_nav_entrypoint.sh
@@ -2,6 +2,9 @@
 
 source /opt/ros/humble/setup.bash
 
+export TURTLEBOT3_MODEL=waffle
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/humble/share/turtlebot3_gazebo/models
+
 ros2 launch src/turtlebot3_bringup/launch/tb3_gz_nav_or_slam.launch.py headless:=$USE_HEADLESS
 
 bash
